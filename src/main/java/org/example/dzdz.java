b8a85b95f4097da123908f206ead580f32eb426c
Buzdin vlad <vvavilon20@mail.ru>
2023-03-10 23:24:55 +0300

    sem1

---
+++ src/main/java/org/example/dzdz.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class dzdz {
+    int[] x = { 1, 1, 0, 1, 0, 0, 1, 1 };
+    for (int i = 0; i < x.lenght; i++);
+        if (x[i] == 0)
+            x[i] = 1;
+    else
+        x[i] = 0
+}
